356dc9a87b807f3a34089667f66ae5847c5ba1a5
maurycywoznica <mjw11@wp.pl>
2022-11-18 22:12:42 +0100

    js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+const btnStart = document.querySelector('.timer__btn-start')
+const btnStop = document.querySelector('.timer__btn-start')
+const btnBrake = document.querySelector('.timer__btn-brake')
+
+const display = document.querySelector('.timer__display-text')
+
+
+
 
 const footerYear = document.querySelector('.footer span')
 footerYear.textContent = `${new Date().getFullYear()}`
@@ -7,14 +15,12 @@ footerYear.textContent = `${new Date().getFullYear()}`
 
 const btnAll= document.querySelectorAll('.timer__btn')
 
-btnAll.forEach(e => {
-  e.addEventListener('click',  element =>{
-    if (e.classList.contains('active'))  {
-      e.classList.toggle('active')
-    }
-  })
-})
+const n1 = '1'
+const n2 = '2'
+const n3 = '3'
+const n4 = '4'
 
 
+display.textContent = `${n1}${n2}:${n3}${n4}`
 
 
