92858e2eeb388dff1fcfe2b2e7b5e86784991eb7
maurycywoznica <mjw11@wp.pl>
2022-11-18 06:42:40 +0100

    add display style

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 "use strict";
 
 
-
-
-
-
 const footerYear = document.querySelector('.footer span')
 footerYear.textContent = `${new Date().getFullYear()}`
+
+
+const btnAll= document.querySelectorAll('.timer__btn')
+
+btnAll.forEach(e => {
+  e.addEventListener('click',  element =>{
+    if (e.classList.contains('active'))  {
+      e.classList.toggle('active')
+    }
+  })
+})
+
+
+
+
